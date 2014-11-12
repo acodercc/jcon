7cf531b03f1ea6896ef4e330daddf2400767383d
CecilLee <cciscecil@gmail.com>
2014-11-12 19:19:58 +0800

    使jFunction函数对象彻底仅包含__fun__成员，从jFunction函数对象派生出parser函数对象，执行派生动作的Parser工厂函数移动回jcon.js文件中

--- src/function.js
+++ src/function.js
@@ -6,33 +6,59 @@
  *  可能的方案是对function进行封装为新的function
  *
  *  另一种方案，我们创建一个jFunction类型的对象
- *  该jFunciton拥有一个_成员，指向要封装的function，使该function没有机会直接被外部调用
- *  只暴露给外部一个call接口，来调用该实际封装起来的命名为_的function
+ *  该jFunciton拥有一个__fun__成员，指向被封装的function，并增加一个约束，外部代码不允许直接访问jfun.__fun__
+ *  使该function没有机会直接被外部调用
  *
- *  那么我们有时机，对该被封装的，命名为_的function进行AOP切面操作，或者链式等内部DSL的增强
+ *  如果想要调用该jfun.__fun__指向的function，仅有的办法是从jfun进行派生，在子对象中，对this.__fun__进行访问
+ *  从而实现对jfun.__fun__的各种封装
+ *
+ *
+ *  如：
+ *
+ *
+     mul2 = jFunction.create({
+        run: function(){
+            return this.__fun__.apply(this, arguments);
+        },
+        map: function(){
+            var rets = [];
+            for(var i=0,len=arguments.length; i<len; i++){
+                rets.push( this.run(arguments[i]) );
+            }
+            return rets;
+        }
+     }).initialize(function(num){
+         return num*2;
+     });
+
+    console.log( mul2.run(2) );
+
+    console.log( mul2.map(2,4) );
+ *
  *
  */
 
 var jFunction = (function(){
 
-    var slice = Array.prototype.slice;
-
+    /**
+     *
+     * jFunction 是自定义的函数对象，该对象封装了一个js的原生function
+     *
+     */
     return jObject.create({
 
         /**
          *
-         */
-        initializer: function(f){
-            this._ = f;
-            return this;
-        },
-
-        /**
+         * @method initialize
+         *
+         * @param {function} f  被封装的函数
          *
          */
-        call: function(self){
-            return this._.apply(slice.call(arguments, 1));
+        initialize: function(f){
+            this.__fun__ = f;
+            return this;
         }
+
     });
 
 }());
--- src/jcon.js
+++ src/jcon.js
@@ -5,15 +5,117 @@
  * A JavaScript parser combinator Library
  *
  */
-(function(global){
+var jcon = (function(undefined){
 
     var slice = Array.prototype.slice;
 
+    function Parser(f){
+
+        return jFunction.create({
+            /**
+             * @method parse
+             *
+             */
+            parse: function(){
+                return this.__fun__.apply(this, arguments);
+            },
+
+            /**
+             * @method seq
+             *
+             * @param {Array:function} arguments      n个将要被以顺序方式组合的解析器
+             *
+             * @desc  进行解析器的顺序组合
+             */
+            seq: function(){
+                var args = slice.call(arguments, 0);
+                args.unshift(this);
+
+                return Parser(function(stream, index){
+                    var currentIndex = index,
+                    values = [],
+                    result,
+                    parserIndex = 0,
+                    parser;
+                    while(parser = args[parserIndex++]){
+                        result = parser.parse(stream, currentIndex);
+                        if(result.success){
+                            currentIndex = result.index;
+                            values.push(result.value);
+                        }else{
+                            return fail(currentIndex, '');
+                        }
+                    }
+                    return success(currentIndex, values);
+                });
+            },
+
+            /**
+             * @method or
+             *
+             * @param {Array:function} arguments        n个选择器，依次尝试匹配，返回第一个成功的
+             *
+             * @desc 进行解析器的或组合
+             */
+            or: function(){
+                var args = slice.call(arguments, 0);
+                args.unshift(this);
+
+                return Parser(function(stream, index){
+                    var parser,
+                    parserIndex = 0;
+                    while(parser = args[parserIndex++]){
+                        result = parser.parse(stream, index);
+                        if(result.success){
+                            return success(result.index, result.value);
+                        }
+                    }
+                    return fail(index, 'in or_parser');
+                });
+            },
+
+            /**
+             * @method times
+             *
+             * @param {function} parse
+             * @param {number} min
+             * @param {number} max
+             *
+             * @desc 在当前输入流上，使用指定的parse进行最少min次，最多max次的匹配
+             *
+             */
+            times: function(min, max){
+                var parser = this;
+                return Parser(function(stream, index){
+                    var successTimes = 0,
+                    values = [];
+
+                    do{
+                        result = parser.parse(stream, index);
+                        if(result.success){
+                            index = result.index;
+                            successTimes++;
+                            values.push(result.value);
+                            if(successTimes === max){
+                                break;
+                            }
+                        }
+                    }while(result.success);
+
+                    if(successTimes >= min && successTimes <= max){
+                        return success(index, values);
+                    }else{
+                        return fail(index, '');
+                    }
+                });
+            }
+        }).initialize(f);
+    }
 
     /**
      * 原子解析器的构造器，基于给定的模式（STR或RE），返回一个进行模式匹配（解析）的函数
      */
-    var generator = jObject.create({
+    return jObject.create({
 
         /**
          * @method string
@@ -172,10 +274,6 @@
 
     });
 
-    global.jcon = generator;
-
-
-
     /**
      * @function success
      * 
@@ -207,6 +305,4 @@
             lastIndex: lastIndex
         };
     }
-
-
-}(this));
+}());
